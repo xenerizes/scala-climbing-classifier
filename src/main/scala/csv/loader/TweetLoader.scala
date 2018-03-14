4a5e0e680459bf7250ca735198c6ccfef7605021
Yuliya <krokodilarseniy@gmail.com>
2018-03-14 23:34:44 +0300

    Loading Sample is more effective

--- src/main/scala/csv/loader/TweetLoader.scala
+++ src/main/scala/csv/loader/TweetLoader.scala
@@ -2,11 +2,11 @@ package csv.loader
 
 import java.io.File
 
-import classifier.sample.{LabeledSample, ObjectClass}
+import classifier.sample.{ObjectClass, Sample}
 import com.github.tototoshi.csv.CSVReader
 
 class TweetLoader(filename: String) {
-  def loadLabeledSample(cl: ObjectClass): LabeledSample = LabeledSample().fromStringList(loadStringList, cl)
+  def loadSample: Sample = Sample().fromStringList(loadStringList)
   def loadStringList: List[String] = loadRaw.flatMap(_.slice(3, 4))
   def loadRaw: List[List[String]] = CSVReader.open(new File(filename))(TweetFormat).all()
 }
